d887c5b36c3035632179072a5973414da5be17ef
Eze Onyebuchi <eze4acme@gmail.com>
2023-07-13 15:58:43 +0100

    update

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import Tours from './Tours'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
 function App() {
+  const [isLoading, SetIsLoading] = useState(true)
   return <>
   <h2>Tours Project Setup</h2>
+ { isLoading ? <Loading /> : <Tours />}
   </>
 }
 
--- src/Tours.js
+++ src/Tours.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Tour from './Tour';
 const Tours = () => {
-  return <h2>tours component</h2>;
+  return <Tour />;
 };
 
 export default Tours;
