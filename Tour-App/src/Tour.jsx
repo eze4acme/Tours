2e34c11b15ea222e25a8fb8e23ee9248334e2b12
Eze Onyebuchi <eze4acme@gmail.com>
2023-07-15 05:13:43 +0100

    update

--- Tour-App/src/Tour.jsx
+++ Tour-App/src/Tour.jsx
@@ -1,7 +1,14 @@
 import React, { useState } from 'react';
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+const Tour = ({id, image, info, price, name}) => {
+  return( <article className="single-tour">
+    <img src={image} alt={name} />
+    <footer>
+      <div className="tour-info">
+        
+      </div>
+    </footer>
+  </article>)
 };
 
 export default Tour;
--- Tour-App/src/Tours.jsx
+++ Tour-App/src/Tours.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import Tour from './Tour';
 const Tours = ({tours}) => {
-  // console.log(tours);
-  {
-    
-  }
-  return <h2>tours component</h2>;
+  return(
+    <section>
+      <div className="title">
+        <h2>Our tours</h2>
+        <div className="underline"></div>
+      </div>
+      <div>
+        {
+          tours.map(tour =>{
+            const {id} = tour
+            return(
+              <Tour key={id} {...tour}/>
+            )
+          })
+        }
+      </div>
+    </section>
+  )
 };
 
 export default Tours;
