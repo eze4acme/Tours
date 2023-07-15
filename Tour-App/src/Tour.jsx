82e9443659287e1017e5c0e15d0493eba438f48e
Eze Onyebuchi <eze4acme@gmail.com>
2023-07-15 05:16:59 +0100

    Update Tour.jsx

--- Tour-App/src/Tour.jsx
+++ Tour-App/src/Tour.jsx
@@ -5,8 +5,12 @@ const Tour = ({id, image, info, price, name}) => {
     <img src={image} alt={name} />
     <footer>
       <div className="tour-info">
-        
+        <h4>{name}</h4>
+        <h4 className="tour-price">
+          ${price}
+        </h4>
       </div>
+        <p>{info}</p>
     </footer>
   </article>)
 };
